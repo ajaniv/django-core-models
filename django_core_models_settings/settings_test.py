9793218492503a3edd9efe80cc9c433723a6d5a6
Amnon Janiv <amnon.janiv@ondalear.com>
2016-05-05 16:57:48 -0400

    disable throttling during unit test

--- django_core_models_settings/settings_test.py
+++ django_core_models_settings/settings_test.py
@@ -9,3 +9,5 @@ from .settings import *  # @UnusedWildImport
 # Uncomment FORCE_DEBUG to see db statements;
 # It also requires change to django.db log level to debug
 # FORCE_DEBUG = True
+# disable throttling during unit tests
+REST_FRAMEWORK['DEFAULT_THROTTLE_CLASSES'] = tuple()
