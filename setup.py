79874bb0e67c2c97aed24dd508ccd8cc3244198f
Amnon Janiv <amnon.janiv@ondalear.com>
2018-09-06 14:00:51 -0400

    29 - upgrade to python3.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 _git_url_root = 'git+ssh://git@github.com/ajaniv/'
 setup(
     name='django-core-models',
-    version='0.3.0',
+    version='0.5.0',
     include_package_data=True,
     license='BSD License',  # example license
     description='A collection of basic reusable concrete Django models',
@@ -28,24 +28,26 @@ setup(
     author_email='amnon.janiv@ondalear.com',
     classifiers=[
         'Environment :: Web Environment',
-        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 1.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     install_requires=[
         'python-core-utils',
         'django-core-utils',
-        'Django>=1.9.0'
+        'Django>=1.11.0'
     ],
     dependency_links=[
-        _git_url_root + 'python-core-utils@v0.3.0#egg=python-core-utils',
-        _git_url_root + 'django-core-utils@v0.3.0#egg=django-core-utils'
+        _git_url_root + 'python-core-utils@v0.5.0#egg=python-core-utils',
+        _git_url_root + 'django-core-utils@v0.5.0#egg=django-core-utils'
     ],
     extras_require={
         'dev': ['check-manifest'],
