0562fa8c677b15e594b527dfd3d6407bf9dfca1d
Amnon Janiv <amnon.janiv@ondalear.com>
2018-09-14 16:03:21 -0400

    disable django debug

--- configs/common/log.py
+++ configs/common/log.py
@@ -71,7 +71,7 @@ LOGGING = {
     'loggers': {
         'django.db': {
             'handlers': ['console', 'log_file'],
-            'level': LOG_LEVEL_DEBUG,
+            'level': LOG_LEVEL_INFO,
             'propagate': True
         },
         'django.request': {
